871c4cefe15fdbf47c047dab8854499bc83db012
muzudre <muzudre@gmail.com>
2016-08-01 23:53:34 +0800

    tap to exit app

--- app/src/main/java/tk/muslimzabirov/html/Main2Activity.java
+++ app/src/main/java/tk/muslimzabirov/html/Main2Activity.java
@@ -3,6 +3,7 @@ package tk.muslimzabirov.html;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -11,8 +12,26 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+
         WebView webView = (WebView)findViewById(R.id.webView2);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("file:///android_asset/index1.html");
     }
+//    double tup to exit from app
+    private static final int TIME_INTERVAL = 2000; // # milliseconds, desired time passed between two back presses.
+    private long mBackPressed;
+
+    @Override
+    public void onBackPressed()
+    {
+        if (mBackPressed + TIME_INTERVAL > System.currentTimeMillis())
+        {
+            super.onBackPressed();
+            return;
+        }
+        else { Toast.makeText(getBaseContext(), "Tap back button in order to exit", Toast.LENGTH_SHORT).show(); }
+
+        mBackPressed = System.currentTimeMillis();
+    }
+//    end of double tapp
 }
